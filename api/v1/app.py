a88ad46a701cccc267d69f1e46c0fb374f97ca8e
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-04-27 22:25:50 +0300

    fix error

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,10 +9,12 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_flask(exception):
     """end event listener"""
     storage.close()
+
 
 if __name__ == "__main__":
     app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
