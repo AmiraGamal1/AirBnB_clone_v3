7e7739c146c7e71f316c47b0768583188095e2dc
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-04-27 20:55:29 +0300

    test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,7 +86,8 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
-    
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_get(self):
         """Test that get returns an object of a given class by id"""
         storage = models.storage
