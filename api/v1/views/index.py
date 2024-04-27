9c6bead8ddcd5ca186a36f5ca9370f22b101835b
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-04-27 22:14:10 +0300

    flask

---
+++ api/v1/views/index.py
@@ -0,0 +1,8 @@
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status')
+def get_status():
+    """get the status of the API"""
+    return jsonify(status='OK')
