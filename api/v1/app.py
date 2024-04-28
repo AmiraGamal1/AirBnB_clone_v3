e148bd3ae0f12540f8b63e34e256d716cf34cf5d
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-04-28 11:02:02 +0300

    404 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """app module"""
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def teardown_flask(exception):
     """end event listener"""
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    """Handles the 404 error"""
+    return jsonify(error='Not found'), 404
 
 
 if __name__ == "__main__":
